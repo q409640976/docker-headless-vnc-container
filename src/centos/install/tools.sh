e71a945f96149784f89804e27381e6afa2835502
q409640976 <q409640976@gmail.com>
2020-04-01 15:28:07 +0800

    Update tools.sh

--- src/centos/install/tools.sh
+++ src/centos/install/tools.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-
+mkdir /tmp/dev
+mkdir /tmp/etc
+mkdir /tmp/home
+mkdir /tmp/opt
+mkdir /tmp/root
+mkdir /tmp/run
+mkdir /tmp/srv
+mkdir /tmp/usr
+mkdir /tmp/var
 echo "Install some common tools for further installation"
 yum -y install epel-release 
 yum -y update
